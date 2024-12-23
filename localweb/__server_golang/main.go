3734416685f074d6551eae28ae1ee363a437cd3c
lost-nd-xxx <shimayoshiba@gmail.com>
2024-12-24 03:04:32 +0900

    ugm_server.exeの多重起動抑止とログファイル生成の改良

--- localweb/__server_golang/main.go
+++ localweb/__server_golang/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -18,11 +17,11 @@ import (
 
 var (
 	kernel32               = syscall.NewLazyDLL("kernel32.dll")
-	procCreateMutexW       = kernel32.NewProc("CreateMutexW")
 	procOpenProcess        = kernel32.NewProc("OpenProcess")
 	procEnumProcesses      = kernel32.NewProc("K32EnumProcesses")
 	procGetModuleBaseNameW = kernel32.NewProc("K32GetModuleBaseNameW")
-	mutex                  syscall.Handle
+	logFile                string
+	logBuffering           string
 )
 
 const (
@@ -30,7 +29,7 @@ const (
 	PROCESS_VM_READ           = 0x0010
 )
 
-// ベースウェアの起動確認に使う
+// ベースウェアの起動確認
 func checkProcess(name string) bool {
 	var processes [1024]uint32
 	var cbNeeded uint32
@@ -56,58 +55,46 @@ func checkProcess(name string) bool {
 	return false
 }
 
-// 多重起動を抑止する
-func createMutex(name string) (syscall.Handle, error) {
-	namePtr, err := syscall.UTF16PtrFromString(name)
-	if err != nil {
-		return 0, err
-	}
-	h, _, err := procCreateMutexW.Call(0, 1, uintptr(unsafe.Pointer(namePtr)))
-	if h == 0 {
-		return 0, err
-	}
-	lastErr := syscall.GetLastError()
-	if lastErr == syscall.ERROR_ALREADY_EXISTS {
-		syscall.CloseHandle(syscall.Handle(h))
-		return 0, fmt.Errorf("ugm_server.exeが既に起動しています。")
-	}
-	return syscall.Handle(h), nil
-}
-
-// ログ設定を初期化
-func initLogger(logFile string) (*os.File, error) {
-	if logFile == "" {
-		// ログ出力を無効化
-		log.SetOutput(nil)
-		return nil, nil
+// ログをファイルへ出力
+func appendLogToFile(message string) {
+	if len(message) > 0 {
+		logBuffering += time.Now().Format(time.DateTime) + ": " + message + "\n"
 	}
 
 	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		return nil, err
+		fmt.Println("ログファイルを開けませんでした: " + err.Error())
+		return
+	}
+	defer file.Close()
+
+	if _, err := file.WriteString(logBuffering); err != nil {
+		fmt.Println("ログ書き込みに失敗しました: " + err.Error())
+		return
 	}
 
-	log.SetOutput(file)
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	return file, nil
+	// バッファをクリア
+	logBuffering = ""
 }
 
 // 設定ファイルを読み込む
-func readConfig(filename string) (string, string, error) {
+func readConfig(filename string) (string, error) {
 	var port = ":8000" // デフォルトポート
-	var logFile string // デフォルトではログ出力無効
 
-	// 実行ファイルのディレクトリを取得し、一つ上のディレクトリを計算
+	// 実行ファイルのディレクトリを取得
 	exePath, err := os.Executable()
 	if err != nil {
-		return port, logFile, fmt.Errorf("実行ファイルパスの取得に失敗しました: %w", err)
+		appendLogToFile("実行ファイルパスの取得に失敗しました: " + err.Error())
+		// 設定ファイルを取得できなかったのでデフォルト値を返す
+		return port, nil
 	}
 	exeDir := filepath.Dir(exePath)
 	parentDir := filepath.Dir(exeDir) // 一つ上のディレクトリ
 
 	file, err := os.Open(filename)
 	if err != nil {
-		return port, logFile, nil // 設定ファイルが存在しない場合でもデフォルト値を返す
+		// 設定ファイルが存在しないのでデフォルト値を返す
+		return port, nil
 	}
 	defer file.Close()
 
@@ -125,101 +112,93 @@ func readConfig(filename string) (string, string, error) {
 			// ポート番号の検査
 			portNumber, err := strconv.Atoi(value)
 			if err != nil || portNumber < 1024 || portNumber > 65535 || portNumber == 9801 || portNumber == 9821 {
-				log.Printf("指定されたポート番号 '%s' は無効または使用できません。デフォルトポート8000を使用します。\n", value)
+				appendLogToFile("無効なポート番号が指定されました。デフォルトポート8000を使用します。")
 				port = ":8000"
 			} else {
 				port = fmt.Sprintf(":%d", portNumber)
 			}
 		case "server_log":
-			// ログファイル名を一つ上のディレクトリに設定
+			// ログファイルを一つ上のディレクトリに設置
 			logFile = filepath.Join(parentDir, value)
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		return port, logFile, err
+		appendLogToFile("設定ファイルの読み込み中にエラーが発生しました: " + err.Error())
+		return port, err
 	}
-	return port, logFile, nil
+	return port, nil
 }
 
 func main() {
 	exePath, err := os.Executable()
 	if err != nil {
-		fmt.Println("実行パスの取得に失敗しました: ", err)
+		appendLogToFile("実行パスの取得に失敗したため、プロセスを終了します: " + err.Error())
 		return
 	}
 	exeDir := filepath.Dir(exePath)
-
-	configFile := filepath.Join(exeDir, "../ugm_config.txt")
-	port, logFile, err := readConfig(configFile)
-	if err != nil {
-		fmt.Println("設定ファイルの読み込みにエラーが発生しました: %w", err)
-		return
-	}
-
-	logOutput, err := initLogger(logFile)
-	if err != nil {
-		fmt.Println("ログの初期化に失敗しました: %w", err)
-		return
-	}
-	if logOutput != nil {
-		defer logOutput.Close()
-	}
-
-	mutex, err = createMutex("UkagakaGhostMessengerLogServer")
-	if err != nil {
-		log.Println("ミューテックスの作成に失敗しました: %w", err)
-		return
-	}
-	defer syscall.CloseHandle(mutex)
-
+	runningFile := filepath.Join(exeDir, "running.signal")
 	shutdownFile := filepath.Join(exeDir, "shutdown.signal")
 
-	if len(os.Args) > 1 && os.Args[1] == "shutdown" {
-		// 終了信号ファイルの作成
-		f, err := os.Create(shutdownFile)
-		if err != nil {
-			log.Println("終了信号ファイルの作成にエラーが発生しました: %w", err)
-			return
-		}
-		f.Close()
-		log.Println("終了信号が送信されました。")
+	// プラグインからの呼び出しであるならrunning.signalは削除済み
+	if _, err := os.Stat(runningFile); err == nil {
+		appendLogToFile("多重起動された可能性があるため、プロセスを終了します。")
 		return
 	}
 
-	// 既存の終了信号ファイルを削除する
-	os.Remove(shutdownFile)
+	// 起動中の信号ファイルを作成する
+	f, err := os.Create(runningFile)
+	f.Close()
+	if err != nil {
+		appendLogToFile("起動中信号ファイルの作成に失敗したため、プロセスを終了します: " + err.Error())
+		return
+	}
+
+	configFile := filepath.Join(exeDir, "../ugm_config.txt")
+	port, err := readConfig(configFile)
+	if err != nil {
+		appendLogToFile("設定ファイルの読み込みにエラーが発生したため、プロセスを終了します: " + err.Error())
+		return
+	}
+
+	// これ以降でプロセスが終了される際、最後に実行される
+	defer func() {
+		appendLogToFile("プロセスを終了します。")
+		// 既存の終了信号ファイルを削除する
+		os.Remove(shutdownFile)
+	}()
+
+	appendLogToFile("サーバーを起動します。")
+	appendLogToFile(fmt.Sprintf("ローカルサーバーをポート%sで起動中...", port))
 
 	htmlDir := filepath.Join(exeDir, "html")
 	fs := http.FileServer(http.Dir(htmlDir))
 	http.Handle("/", fs)
 
-	srv := &http.Server{
-		Addr: port,
-	}
+	srv := &http.Server{Addr: port}
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
-
 	done := make(chan bool, 1)
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Println("サーバーの起動にエラーが発生しました: %w", err)
+			appendLogToFile("サーバーの起動に失敗しました: " + err.Error())
+			done <- true
 			return
 		}
 	}()
 
 	go func() {
 		for {
-			time.Sleep(10 * time.Second)
+			time.Sleep(1 * time.Second)
 			if !checkProcess("ssp.exe") {
-				log.Println("ssp.exeが見つかりません。サーバーを終了しています。")
+				appendLogToFile("ssp.exeが見つかりません。サーバーを終了しています。")
 				srv.Shutdown(context.Background())
 				done <- true
 				return
 			}
 			if _, err := os.Stat(shutdownFile); err == nil {
-				log.Println("終了信号ファイルが検出されたため、サーバーを終了しています。")
+				appendLogToFile("終了信号ファイルが検出されたため、サーバーを終了しています。")
 				srv.Shutdown(context.Background())
 				done <- true
 				return
@@ -232,8 +211,13 @@ func main() {
 	case <-done:
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// サーバー停止
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
-	srv.Shutdown(ctx)
-	log.Println("サーバーを終了しました。")
+	if err := srv.Shutdown(ctx); err != nil {
+		appendLogToFile("サーバーの停止に失敗しました: " + err.Error())
+		return
+	}
+
+	appendLogToFile("サーバーを終了しました。")
 }
